afd2aefcffd74a3bed6509b61075293fda9ec251
magnolia <magnolia.k@me.com>
2015-01-25 23:15:52 +0900

    Replace testing framework to specs2

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ lazy val root = (project in file(".")).
             name := "PerlVersions",
             version := "1.0",
             scalaVersion := "2.11.4",
-            libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.3" % "test",
+            libraryDependencies += "org.specs2" %% "specs2-core" % "2.4.15" % "test",
             libraryDependencies += "net.databinder.dispatch" %% "dispatch-core" % "0.11.2",
             libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.9"
 
--- src/test/scala/PerlVersionTest.scala
+++ src/test/scala/PerlVersionTest.scala
@@ -1,21 +1,31 @@
 package com.github.magnolia_k.perl_version
 
-import org.scalatest.FunSuite
+import org.specs2._
 
-class PerlVersionTest extends FunSuite {
-    test("Invalid param pattern") {
-        try {
-            val invalid = new PerlVersion("a.bc.d")
-        } catch {
-            case _: java.lang.IllegalArgumentException =>
-        }
-    }
+class PerlVersionSpec extends Specification {
+  
+  val version = new PerlVersion("5.20.1")
 
-    test("Valid param pattern") {
-        val version = new PerlVersion("5.20.1")
+  def is = s2"""
 
-        assert(version.ver == "5.20.1")
-        assert(version.isStable == true)
-        assert(version.url == "http://www.cpan.org/src/5.0/perl-5.20.1.tar.gz")
-    }
+これはPerlVersionの仕様です。
+
+PerlVersionは…
+  a.bc.cはダメなバージョン形式  $e1
+
+  5.20.1は
+    妥当なバージョン      $e2
+    安定バージョン        $e3
+    URLは正しく取得 ${version.url}      $e4
+"""
+
+
+  def e1 = {
+      val invalid = new PerlVersion("a.bc.d")
+    } must throwA[java.lang.IllegalArgumentException]
+
+  def e2 = version.ver must contain("5.20.1")
+  def e3 = version.isStable must beTrue
+  def e4 = version.url must
+    contain("http://www.cpan.org/src/5.0/perl-5.20.1.tar.gz")
 }
